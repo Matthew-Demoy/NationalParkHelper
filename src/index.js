b1884de0e059072e9294aa6e55b02af69c5e4dac
Matthew Demoy <Matthew_Demoy@student.uml.edu>
2019-10-02 23:52:23 -0400

    Title Screen Almost Complete
    Learned about
    masks
    svg
    fonts
    flexbox

--- src/index.js
+++ src/index.js
@@ -8,16 +8,67 @@ import USAMap from "./map.js";
 import SVG from "react-inlinesvg";
 
 import LogoComponent from "./logo-svg.js";
+import DisplayTable from "./displayTable.js";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>United States National Parks</h1>
-      <LogoComponent />
-      <h2>Select one or more states to view</h2>
-      <USAMap />
-    </div>
-  );
+//https://www.nationalparks.org/explore-parks
+import "../fonts/Brandon_blk.otf";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedStates: []
+    };
+  }
+
+  sendParks = dataFromChild => {
+    console.log("data" + dataFromChild);
+    this.setState({ selectedStates: dataFromChild });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <div class="top">
+          <div className="bg-img" />
+          <div className="saturate-layer" />
+          <div className="bg-gradient" />
+
+          <div className="topContainer">
+            <div id="topLogoContainer">
+              <LogoComponent />
+            </div>
+
+            <div className="verticalContainer">
+              <div className="arrowContainer">
+                <h1 className="grit">EXPLORE PARKS</h1>
+              </div>
+
+              <h3>
+                Explore America’s national parks. Discover our most treasured
+                places, supported by people like you, and start your travel
+                planning here.
+              </h3>
+            </div>
+
+            <div id="donateButton">
+              <a
+                id="donateLink"
+                href="https://donate.nationalparks.org/page/40194/donate/1?_ga=2.81891414.1731577169.1569891660-285251361.1568148816"
+              >
+                Donate to the Parks
+              </a>
+            </div>
+          </div>
+
+          <USAMap callBackParent={this.sendParks} />
+        </div>
+        <div>
+          <DisplayTable displayedParks={this.state.selectedStates} />
+        </div>
+      </div>
+    );
+  }
 }
 
 const rootElement = document.getElementById("root");
